5ad6cf62206ab8393fcebe7cab7e0b5b068e90c5
Wojciech Siewierski <darkvifon@gmail.com>
2013-10-29 18:44:35 +0100

    doc preview with antiword

--- scope.sh
+++ scope.sh
@@ -65,6 +65,9 @@ case "$extension" in
         try lynx   -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
         try elinks -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
         ;; # fall back to highlight/cat if the text browsers fail
+    # Word Documents
+    doc)
+        try antiword "$path" && { dump | trim; exit 0; } || exit 1;;
 esac
 
 case "$mimetype" in
