3baa05fef8fac9422d677bed7d9e6d814dbb4130
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2015-04-19 23:27:30 +0200

    ranger: fzf_select

--- topdir/ranger/.config/ranger/commands.py
+++ topdir/ranger/.config/ranger/commands.py
@@ -8,6 +8,7 @@
 # You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import *
 
+import os.path
 
 class shell(Command):
     escape_macros_for_shell = True
@@ -110,15 +111,33 @@ class fasd(Command):
             directory = subprocess.check_output(["fasd", "-d"]+arg.split(), universal_newlines=True).strip()
             self.fm.cd(directory)
 
-class fzfcd(Command):
+class fzf_select(Command):
+    """
+    :fzf_select
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
     def execute(self):
-        command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-                 -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        if self.quantifier:
+            # match only directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        else:
+            # match files and directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
         fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
-            directory = stdout.decode('utf-8').rstrip('\n')
-            self.fm.cd(directory)
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
 
 
 import signal
