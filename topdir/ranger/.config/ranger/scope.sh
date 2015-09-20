e9e62003c8ddbc37ac26ad82e877e92ce9f4c860
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2015-09-20 17:14:50 +0200

    ranger: Merge scope.sh with the upstream

--- topdir/ranger/.config/ranger/scope.sh
+++ topdir/ranger/.config/ranger/scope.sh
@@ -17,18 +17,20 @@
 # 4    | fix height | success. Don't reload when height changes
 # 5    | fix both   | success. Don't ever reload
 # 6    | image      | success. display the image $cached points to as an image preview
+# 7    | image      | success. display the file directly as an image
 
 # Meaningful aliases for arguments:
-path="$1"    # Full path of the selected file
-width="$2"   # Width of the preview pane (number of fitting characters)
-height="$3"  # Height of the preview pane (number of fitting characters)
-cached="$4"  # Path that should be used to cache image previews
+path="$1"            # Full path of the selected file
+width="$2"           # Width of the preview pane (number of fitting characters)
+height="$3"          # Height of the preview pane (number of fitting characters)
+cached="$4"          # Path that should be used to cache image previews
+preview_images="$5"  # "True" if image previews are enabled, "False" otherwise.
 
 maxln=200    # Stop after $maxln lines.  Can be used like ls | head -n $maxln
 
 # Find out something about the file:
 mimetype=$(file --mime-type -Lb "$path")
-extension=$(/bin/echo -E "${path##*.}" | tr "[:upper:]" "[:lower:]")
+extension=$(/bin/echo "${path##*.}" | tr "[:upper:]" "[:lower:]")
 
 # Functions:
 # runs a command and saves its output into $output.  Useful if you need
@@ -36,13 +38,41 @@ extension=$(/bin/echo -E "${path##*.}" | tr "[:upper:]" "[:lower:]")
 try() { output=$(eval '"$@"'); }
 
 # writes the output of the previously used "try" command
-dump() { /bin/echo -E "$output"; }
+dump() { /bin/echo "$output"; }
 
 # a common post-processing function used after most commands
 trim() { head -n "$maxln"; }
 
 # wraps highlight to treat exit code 141 (killed by SIGPIPE) as success
 highlight() { command highlight "$@"; test $? = 0 -o $? = 141; }
+
+# Image previews, if enabled in ranger.
+if [ "$preview_images" = "True" ]; then
+    case "$mimetype" in
+        # Image previews for SVG files, disabled by default.
+        image/svg+xml)
+          convert "$path" "$cached" && exit 6 || exit 1;;
+
+        # Image previews for image files. w3mimgdisplay will be called for all
+        # image files (unless overriden as above), but might fail for
+        # unsupported types.
+        image/*)
+            exit 7;;
+
+        # Image preview for video, disabled by default.:
+        video/*)
+           ffmpegthumbnailer -i "$path" -o "$cached" -s 0 && exit 6 || exit 1;;
+
+        # # GraphViz Graphs (may be slow!):
+        # dot)
+        #     dot -Tpng "$path" > "$cached" && exit 6 || exit 1;;
+
+        # PDF documents:
+        pdf)
+            try pdftotext -l 10 -nopgbrk -q "$path" - && \
+                { dump | trim | fmt -s -w $width; exit 0; } || exit 1;;
+    esac
+fi
 
 case "$extension" in
     # Archive extensions:
@@ -54,10 +84,10 @@ case "$extension" in
         exit 1;;
     rar)
         try unrar -p- lt "$path" && { dump | trim; exit 0; } || exit 1;;
-    # # PDF documents:
-    # pdf)
-    #     try pdftotext -l 10 -nopgbrk -q "$path" - && \
-    #         { dump | trim | fmt -s -w $width; exit 0; } || exit 1;;
+    # PDF documents:
+    pdf)
+        try pdftotext -l 10 -nopgbrk -q "$path" - && \
+            { dump | trim | fmt -s -w $width; exit 0; } || exit 1;;
     # BitTorrent Files
     torrent)
         try transmission-show "$path" && { dump | trim; exit 5; } || exit 1;;
@@ -67,9 +97,6 @@ case "$extension" in
         try lynx   -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
         try elinks -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
         ;; # fall back to highlight/cat if the text browsers fail
-    # GraphViz Graphs (may be slow!):
-    dot)
-        dot -Tpng "$path" > "$cached" && exit 6 || exit 1;;
 esac
 
 case "$mimetype" in
@@ -79,16 +106,11 @@ case "$mimetype" in
     # Ascii-previews of images:
     image/*)
         img2txt --gamma=0.6 --width="$width" "$path" && exit 4 || exit 1;;
-    # Image preview for videos, disabled by default:
-    video/*)
-        ffmpegthumbnailer -i "$path" -o "$cached" -s 0 && exit 6 || exit 1;;
     # Display information about media files:
     video/* | audio/*)
         exiftool "$path" && exit 5
         # Use sed to remove spaces so the output fits into the narrow window
         try mediainfo "$path" && { dump | trim | sed 's/  \+:/: /;';  exit 5; } || exit 1;;
-    application/pdf)
-        pdftoppm -f 1 -l 1 -scale-to-x 1680 -scale-to-y -1 -singlefile -jpeg -tiffcompression jpeg "$path" "${cached%.*}" && exit 6 || exit 1;;
 esac
 
 exit 1
