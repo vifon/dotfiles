0a3c36da55d1fd40b3e41317a4aafffc748eb9be
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2015-10-04 21:11:18 +0200

    xbindkeys: Port the config to Scheme

---
+++ topdir/X/.xbindkeysrc.scm
@@ -0,0 +1,136 @@
+;; Shell command key:
+;; (xbindkey key "foo-bar-command [args]")
+;; (xbindkey '(modifier* key) "foo-bar-command [args]")
+;;
+;; Scheme function key:
+;; (xbindkey-function key function-name-or-lambda-function)
+;; (xbindkey-function '(modifier* key) function-name-or-lambda-function)
+;;
+;; Other functions:
+;; (remove-xbindkey key)
+;; (run-command "foo-bar-command [args]")
+;; (grab-all-keys)
+;; (ungrab-all-keys)
+;; (remove-all-keys)
+;; (debug)
+
+(define (bindkeys command . keys)
+  (let ((bind-func (if (string? command)
+                       xbindkey
+                       xbindkey-function)))
+    (for-each (lambda (key) (bind-func key command))
+              keys)))
+
+(bindkeys "CLEARONSTART=1 urxvtcd"
+          '(mod4 space))
+
+(bindkeys "emacsclient -c"
+          '(mod4 e))
+(bindkeys "emacs"
+          '(mod4 control e))
+(bindkeys "thunderbird"
+          '(mod4 F9))
+
+(bindkeys "zsh -c 'if (( $(xbacklight -get) < 9 )); then xbacklight -set 10; else xbacklight -inc 10; fi'"
+          'XF86MonBrightnessUp)
+(bindkeys "zsh -c 'if (( $(xbacklight -get) > 10 )); then xbacklight -dec 10; else xbacklight -set 1; fi'"
+          'XF86MonBrightnessDown)
+(bindkeys "xbacklight -set 100"
+          '(mod4 XF86MonBrightnessUp))
+(bindkeys "xbacklight -set 1"
+          '(mod4 XF86MonBrightnessDown))
+(bindkeys "notify-send `xbacklight -get`"
+          '(mod4 control XF86MonBrightnessUp))
+
+(bindkeys "rox"
+          '(mod4 control grave))
+
+(bindkeys "rofi -show window"
+          '(mod4 n))
+
+(bindkeys "kvm-switch"
+          'Scroll_Lock)
+
+(bindkeys "chromium $(xclip -o)"
+          '(mod4 mod1 o))
+
+(bindkeys "wxmaxima"
+          'XF86Calculator)
+(bindkeys "urxvtcd -e sh -c '/usr/bin/locate \"\" | fzf | tr -d \"\\n\" | xsel -i'"
+          'XF86Search)
+(bindkeys "eject /dev/sr0"
+          'XF86Eject)
+(bindkeys "i3lock-blur"
+          'XF86ScreenSaver
+          '(control shift mod4 l)
+          '(mod5 shift mod4 l))
+
+(bindkeys "urxvtcd -g 150x32 -e ncmpcpp-run"
+          '(mod4 m))
+(bindkeys "amixer -D pulse set Master Playback Switch toggle &> /dev/null && sleep 0.1 && killall -USR1 i3status"
+          'XF86AudioMute)
+(bindkeys "amixer set Speaker toggle &> /dev/null && sleep 0.1 && killall -USR1 i3status"
+          '(mod1 XF86AudioMute))
+(bindkeys "amixer set Capture toggle"
+          'XF86AudioMicMute)
+(bindkeys "amixer -D pulse set Master 5%- && killall -USR1 i3status"
+          'XF86AudioLowerVolume)
+(bindkeys "amixer -D pulse set Master 5%+ && killall -USR1 i3status"
+          'XF86AudioRaiseVolume)
+(bindkeys "mpcauth -q  prev"
+          '(mod4 control comma)
+          'XF86AudioPrev)
+(bindkeys "mpcauth -q  next"
+          '(mod4 control period)
+          'XF86AudioNext)
+(bindkeys "mpcauth -q  stop"
+          '(mod4 control slash)
+          'XF86AudioStop)
+(bindkeys "mpcauth -q  toggle"
+          '(mod4 grave)
+          '(mod4 slash)
+          '(mod4 Escape)
+          'XF86AudioPlay)
+
+(bindkeys "urxvtcd -title IRC -e wbd"
+          '(mod4 F10))
+(bindkeys "urxvtcd -title 'IRC [mosh]' -e wbd --mosh"
+          '(mod4 control F10))
+
+(bindkeys "dmenu_launcher"
+          '(mod4 y))
+
+(bindkeys "dmenu_translate -g"
+          '(mod4 backslash))
+(bindkeys "dmenu_translate -g -f pl -t en"
+          '(mod4 shift backslash))
+(bindkeys "zsh -c 'urxvt -e less -R =(trans en:pl \\\"`xsel -o`\\\")'"
+          '(mod4 control backslash))
+
+(bindkeys "scrot-upload"
+          '(release Print))
+(bindkeys "scrot-upload 1"
+          '(release mod4 Print))
+(bindkeys "scrot -s"
+          '(release control Print))
+(bindkeys "scrot -s --exec 'mtpaint $f; rm -f $f'"
+          '(release mod5 Print))
+
+(bindkeys "~/.startup 1"
+          '(mod4 Insert))
+(bindkeys "~/.startup --reset-mouse"
+          '(mod4 control Insert))
+(bindkeys "~/.startup 1 && xmodmap -e 'remove control = Control_L' -e 'keycode 37 = Super_L' -e 'add control = Control_L' -e 'add mod4 = Super_L'"
+          '(mod1 control Insert))
+
+(bindkeys "compton-trans -c -o -20"
+          '(mod4 control minus))
+(bindkeys "compton-trans -c -o +20"
+          '(mod4 control equal))
+
+(bindkeys "redshift -O 3700"
+          '(mod4 shift Right))
+(bindkeys "redshift -O 4500"
+          '(mod4 shift Down))
+(bindkeys "redshift -x"
+          '(mod4 shift Left))
