64ff9483d0ccc7d4fafa807a03c2b8d3ab87e5d3
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2019-07-09 01:15:18 +0200

    Move parts of xinitrc to separate Bash sourcables

---
+++ topdir/ssh/.bin/ssh-agent-start-or-attach.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script must be sourced.
+
+if SSH_AGENT_PID=$(pgrep --uid $(whoami) --exact ssh-agent | head -n1); then
+    SSH_AUTH_SOCK=$(printf '%s' /tmp/ssh*/agent.$((SSH_AGENT_PID - 1)))
+    export SSH_AGENT_PID
+    export SSH_AUTH_SOCK
+else
+    eval $(ssh-agent)
+fi
